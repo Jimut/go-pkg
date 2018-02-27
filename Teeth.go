69cd41f66997884f71b1d003418b23c6a5e30ff2
Jimut Dhali <jemdhali67@gmail.com>
2018-02-27 13:22:15 +0530

    added code to read json file

--- Teeth.go
+++ Teeth.go
@@ -1,9 +1,29 @@
 package Alligator
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 type Teeth struct {
-  Enamel string
+	Enamel string
 }
 
-func GetTeeth () string {
-  return "No Teeth"
+func GetTeeth() string {
+	return "No Teeth"
+}
+
+func GetMeta() map[string]int {
+	buff, err := ioutil.ReadFile("data/meta.json")
+	check(err)
+
+	data := make(map[string]int)
+	json.Unmarshal(buff, &data)
+	return data
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
